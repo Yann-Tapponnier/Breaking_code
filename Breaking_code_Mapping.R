4e77122a7f0e50dbe3225dfc112549f40d209a3e
Yann.Tapponnier <165476928+Yann-Tapponnier@users.noreply.github.com>
2026-05-13 09:26:01 +0200

    Update Breaking_code_Mapping.R
    Adding lots of variation
    Trouble shooting
    SCTtransform VS LogNormalized

--- Breaking_code_Mapping.R
+++ Breaking_code_Mapping.R
@@ -21,48 +21,236 @@ TransferData() is used to transfer cell type labels and impute the ADT values;
 IntegrateEmbeddings() is used to integrate reference with query by correcting the query’s projected low-dimensional embeddings; 
 ProjectUMAP() is used to project the query data onto the UMAP structure of the reference. 
 
+############################### Dependencies 
+library(future)
+options(future.globals.maxSize = 10000 * 1024^2)  # 10 GiB
 
+install.packages('BiocManager')
+BiocManager::install('glmGamPoi')
 
 ###############################  Coding using MAPQUERY #################################
 # Import both dataset in a single session
 reference # In Vivo
 query  # In Vitro
 
+
 ##### Re-running the basic 
 reference <- reference %>% 
-  NormalizeData %>% # by default takes it all
+  NormalizeData %>% # by default "logNormalize" takes it all 
   FindVariableFeatures %>% # 2000 by default
   ScaleData(vars.to.regress = c('nCount_RNA', 'percent.mt',"CC.diff")) %>% # by default use the FindVariableFeatures
   RunPCA %>% 
-  RunUMAP( dims = 1:50, return.model = T) # return.model = MANDATORY !
+  RunUMAP( dims = 1:50, return.model = T) # return.model = MANDATORY only for REFERENCE (it is the space where query will be projected) !
 
-
-query <- query %>% 
-  NormalizeData %>% # by default takes it all
-  FindVariableFeatures %>% # 2000 by default
-  ScaleData(vars.to.regress = c('nCount_RNA', 'percent.mt',"CC.diff")) %>% # by default use the FindVariableFeatures
-  RunPCA %>% 
-  RunUMAP( dims = 1:50, return.model = T) # return.model = MANDATORY !
+                      ############# Not necessary ###############
+                      # query <- query %>% 
+                      #   NormalizeData %>% # by default takes it all
+                      #   FindVariableFeatures %>% # 2000 by default
+                      #   ScaleData(vars.to.regress = c('nCount_RNA', 'percent.mt',"CC.diff")) %>% # by default use the FindVariableFeatures
+                      #   RunPCA %>% 
+                      #   RunUMAP( dims = 1:50, return.model = T) 
 
 anchors <- FindTransferAnchors(
   reference = reference,
   query = query,
-  reference.reduction = "pca"
+  normalization.method = "LogNormalize",  #Default but can be SCT
+  reference.reduction = "pca",
+  dims = 1:30  # 1:50 c'est beaucoup, 30 est standard
 )
 
 
 # by orig.ident
-# query <- MapQuery(anchorset = anchors, reference = reference, query = query,
-#     refdata = reference$orig.ident, 
-#     reference.reduction = "pca", 
-#     reduction.model = "umap")
+query <- MapQuery(anchorset = anchors,
+                  reference = reference, 
+                  query = query,
+                  refdata = reference$orig.ident,
+                  reference.reduction = "pca",
+                  reduction.model = "umap") # Can be NOT added to have the LABEL TRANSFERT
 
 # by BAM labeling
-query <- MapQuery(anchorset = anchors, reference = reference, query = query,
-                  refdata = reference$BAM, 
-                  reference.reduction = "pca", 
-                  reduction.model = "umap")
+# query <- MapQuery(anchorset = anchors, 
+#                   reference = reference,
+#                   query = query,
+#                   refdata = reference$BAM, 
+#                   reference.reduction = "pca", 
+#                   reduction.model = "umap")
 
+
+
+
+################### Different Normalization : SCT VS LogNormalized ############################
+Ex : reference (from a paper) is SCT transformed a
+     query (my data) is LogNormalize
+   
+# If reference have both : reference@assay :  "RNA", "SCT"  
+# You can do Choice 1 or 2 !!!   )))
+     
+## Chemin 2 — SCTransformer ta query (plus rigoureux car on ne modifie pas la ref qui vient d'un papier)
+query <- SCTransform(query, vst.flavor = "v2", variable.features.n = 2000) # remplace findVariableFeature NormalizeData and scaleData / Default 2000
+DefaultAssay(query) <- "SCT"
+query <- RunPCA(query)
+# Pas besoin de RunUMAP sur la query !
+
+anchors <- FindTransferAnchors(
+  reference = reference,
+  query = query,
+  normalization.method = "SCT",
+  reference.reduction = "pca",
+  dims = 1:30
+)
+            
+query <- MapQuery(
+  anchorset = anchors,
+  reference = reference,
+  query = query,
+  referencedata = list(celltype = "celltype"),
+  reference.reduction = "pca",
+  reduction.model = "umap"
+)
+
+
+###################. Trouble Shooting ############################
+
+                    ################### Different Normalization : SCT VS LogNormalized ############################
+                    Ex : reference (from a paper) is SCT transformed a
+                    query (my data) is LogNormalize
+                    
+                    # If reference have both : reference@assay :  "RNA", "SCT"  
+                    # You can do Choice 1 or 2 !!!   )))
+                    
+                    ## Chemin 2 — SCTransformer ta query (plus rigoureux car on ne modifie pas la ref qui vient d'un papier)
+                    variable.features.n = 2000) # remplace findVariableFeature NormalizeData and scaledata
+                     DefaultAssay(query) <- "SCT"
+                    query <- RunPCA(query)
+                    # Pas besoin de RunUMAP sur la query !
+
+                              # ERREUR --> Checking
+                              # 1. Features communes
+                              common_features <- intersect(rownames(reference@assays$SCT), 
+                                                           rownames(query@assays$SCT))
+                              length(common_features)  # combien ?
+                              
+                              # 2. Vérifier le PCA de la ref
+                              dim(reference@reductions$pca@cell.embeddings)
+                              
+                              # 3. Vérifier les variable features de la ref
+                              length(VariableFeatures(reference))
+                              
+                              # 4. Vérifier les variable features de la query
+                              sum(VariableFeatures(reference) %in% rownames(query@assays$SCT))
+                              
+                              # Le SCT de la ref a bien une scale.data ?
+                              dim(reference@assays$SCT@scale.data)
+                              
+                              # Le PCA de la ref vient bien de SCT ?
+                              reference@reductions$pca@assay.used
+                              
+                              # La query a bien un SCT avec scale.data ?
+                              dim(query@assays$SCT@scale.data)
+                              
+                              # DefaultAssay des deux ?
+                              DefaultAssay(reference)
+                              DefaultAssay(query)
+                              
+                              # Les 2000 features du SCT ref sont bien dans le scale.data de la query ? 531 in commun = too few
+                              sum(rownames(reference@assays$SCT@scale.data) %in% 
+                                    rownames(query@assays$SCT@scale.data))
+                              
+                  # 531 in commun = too few
+                              
+                  #  Increasing variable feature of query to 3000
+                  query <- SCTransform(query, vst.flavor = "v2", variable.features.n = 3000) # remplace findVariableFeature NormalizeData and scaleData
+                  DefaultAssay(query) <- "SCT"
+                  query <- RunPCA(query) 
+                  
+                  # Still failing with 3000 variable features in the query (but more in commun with the ref : 531)              
+                  
+                    anchors <- FindTransferAnchors(
+                                reference = reference,
+                                query = query,
+                                normalization.method = "SCT",
+                                reference.reduction = "pca",
+                                dims = 1:30,
+                                features = VariableFeatures(reference), # ici on force l'utilisation des variable features de la ref (et pas de la query)
+                                verbose = T
+                              )
+                              
+                              query <- MapQuery(
+                                anchorset = anchors,
+                                reference = reference,
+                                query = query,
+                                referencedata = list(celltype = "celltype"),
+                                reference.reduction = "pca",
+                                reduction.model = "umap"
+                              )
+                        
+
+
+## Chemin 2 — Forcer RNA (rapide, à tester en premier)
+                        
+## Chemin 2 — Forcer RNA (rapide, à tester en premier)
+# Ref — recalculer sur RNA
+DefaultAssay(reference) <- "RNA"
+reference <- reference %>%
+  NormalizeData %>%
+  FindVariableFeatures %>%
+  ScaleData() %>% #ScaleData(vars.to.regress = c('nCount_RNA', 'percent.mt', "CC.diff")) %>%
+  RunPCA %>%
+  RunUMAP(dims = 1:30, return.model = TRUE)
+
+# Query — pipeline LogNormalize
+DefaultAssay(query) <- "RNA"
+query <- query %>%
+  NormalizeData %>%
+  FindVariableFeatures %>%
+  ScaleData(vars.to.regress = c('nCount_RNA', 'percent.mt', "CC.diff")) %>%
+  RunPCA
+
+# Anchors
+anchors <- FindTransferAnchors(
+  reference = reference,
+  query = query,
+  normalization.method = "LogNormalize",
+  reference.reduction = "pca",
+  dims = 1:30
+)
+
+# MapQuery
+query <- MapQuery(
+  anchorset = anchors,
+  reference = reference,
+  query = query,
+  refdata = reference$seurat_clusters,
+  reference.reduction = "pca",
+  reduction.model = "umap"
+)
+
+
+
+###################### NO UMAP recalculation  : Transfert CellTypes and Clusters without Projection !!!
+tu veux juste transférer les labels (celltype, clusters...)
+
+# Retire simplement reduction.model de MapQuery()
+query <- MapQuery(
+  anchorset = anchors,
+  reference = ref,
+  query = query,
+  refdata = list(celltype = "celltype"),
+  reference.reduction = "pca"
+  # pas de reduction.model ici
+)
+
+
+Pas besoin de RunUMAP du tout dans ce cas. ✅
+
+ObjectifRunUMAP nécessaire ?Transférer labels seulement❌ NonProjeter query sur UMAP ref✅ Oui (avec return.model = TRUE)
+Tu veux faire quoi exactement — juste le transfert de labels, ou aussi la projection UMAP ?
+  
+
+
+
+  
+  
 ################################# IF MapQuery do not work DO THAT instead : ######################################
 
 # IF MapQuery do not work :  
@@ -108,70 +296,6 @@ p2 <- DimPlot(query, reduction = "ref.umap", group.by = "BAM", label = TRUE,
 png(filename = paste0("report/3_NBEI1_integration_clustering_DOX/Mapping_In_Vivo_on_In_VITRO_4.png"), width = 2400, height = 900, res=100 )
 print(p2)
 dev.off()
-
-
-
-
-################################# Azimuth for Atlas ref ######################################
-BE CAREFULL if cell in the query dataset that are not represented in the reference,
-they will project to the MOST SIMILAR cell in the reference.  SO IT CAN MASK THE DISCOVERY of NEW CELLTYPE
-
-https://satijalab.github.io/azimuth/articles/run_azimuth_tutorial.html
-
-
-# install Azimuth directly from main branch
-update.packages(oldPkgs = c("withr", "rlang"))
-#remotes::install_github('satijalab/azimuth', ref = 'master')
-
-devtools::install_github("satijalab/seurat-data", "seurat5")
-
-                        # #library(Azimuth) DOES NOT WORK !!!
-                        # # returns a Seurat object named fetusref
-                        # #fetusref <- LoadData("fetusref")
-                        # #data("fetusref")
-                        # # Get SeuratData installation path
-                        # 
-                        # # The RunAzimuth function can take a Seurat object as input
-                        # fetusref <- RunAzimuth(fetusref, reference = "fetusref")
-                        
-                        # remove.packages(c("SeuratData", "Azimuth", "rappdirs"))
-                        # .libPaths()  # shows your library paths
-                        # 
-                        # install.packages("rappdirs")        # fix the corrupt dependency first
-                        # install.packages("Seurat")          # core Seurat package
-                        # install.packages("SeuratData")      # Seurat reference datasets
-
-library(Seurat)
-library(SeuratData)
-
-# Install dataset you want
-Avdata <- AvailableData()
-options(timeout = 1800)  # increase timeout to 30 minutes
-
-InstallData("fetusref") # Installing human feotus ref for having neurblast + schwan cells
-OR download directly through Zenodo --> There is the .RDS files !!
-  
-
-# check all installed datasets
-available <- InstalledData()
-available[grep("fetus", available$Dataset), ]
-
-
-# FINDING the folder of installed terminal : 
-system()::"locate" fetusref.DeuratData
-setwd("/Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/library/fetusref.SeuratData")  # Set working directory up to the file that contains
-
-# your .rds and .rdb files. 
-fetus <- readRDS("azimuth/ref.rds")
-
-DimPlot(fetus,group.by = "annotation.l1", label = TRUE)  + NoLegend()
-DimPlot(fetus,group.by = "annotation.l2", label = TRUE)  + NoLegend()
-
-DimPlot(reduction= "refDR" ,fetus,group.by = "annotation.l1", label = TRUE)  + NoLegend() # It is PCA redcution.
-
-
-
-
 
 
 
